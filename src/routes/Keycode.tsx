99d613315e6479288cc9428d3d3b3214ef57280c
bjtj <bjtj10@gmail.com>
2023-10-07 08:35:48 +0900

    keycode history

--- src/routes/Keycode.tsx
+++ src/routes/Keycode.tsx
@@ -5,17 +5,40 @@ type KeycodeProps = {
   
 };
 
+type KeycodeHistItem = {
+  key: string;
+  keyCode: number;
+};
+
 export default function Keycode({ }:KeycodeProps) {
 
   const [keycode, setKeycode] = useState<string>();
+  const [keycodeHist, setKeycodeHist] = useState<KeycodeHistItem[]>([]);
+
+  function handleKeycode(item: KeycodeHistItem) {
+    setKeycode(`${item.key} (keycode: ${item.keyCode})`);
+    setKeycodeHist(prev => {
+      return [item, ...prev].slice(0, 100);
+    });
+  }
   
   return (
     <div>
       <h1>Keycode</h1>
       <Input className="font-mono min-w-[20em] text-center" type="text"
         value={keycode}
-        onKeyDown={(e) => { setKeycode(`${e.key} (keycode: ${e.keyCode})`) }}
+        onKeyDown={(e) => {
+          e.preventDefault();
+          handleKeycode(e);
+        }}
         placeholder="Type any key..." />
+      <ul>
+        {
+          keycodeHist.map((item, i) => (
+            <li key={`hist-item-${i}`}><pre>{`${item.key} (keycode: ${item.keyCode})`}</pre></li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
