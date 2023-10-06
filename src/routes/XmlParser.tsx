342433ade10907ad028a1951ea3530b603ec4087
bjtj <bjtj10@gmail.com>
2023-10-06 10:55:03 +0900

    xml parser 만들기

--- src/routes/XmlParser.tsx
+++ src/routes/XmlParser.tsx
@@ -1,12 +1,35 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
+import Button from '../components/Button';
+
+const parser = new DOMParser();
+
 export default function XmlParser() {
 
   const [text, setText] = useState<string>();
+  const [error, setError] = useState<string>();
+
+  const parse = useCallback(() => {
+    if (text) {
+      try {
+      let ret = parser.parseFromString(text, 'application/xml');
+        console.log(ret);
+      } catch (err) {
+        setError(`${err}`);
+      }
+    }
+  }, [text]);
 
   return (
     <div>
       <h1>Xml Parser</h1>
-      <div>{text}</div>
+      <textarea className="w-full min-h-[450px] border font-mono text-sm" value={text} onChange={e => setText(e.target.value)} />
+      <Button onClick={parse}>Parse</Button>
+      { error && (
+          <>
+            <h2>Error:</h2>
+            <div className="border border-red-600 bg-red-300/10 text-red-600 p-3">{error}</div>
+          </>)
+      }
     </div>
   );
 }
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -11,29 +11,47 @@ export async function action() {
 }
 
 export default function Root() {
-  return (
-    <div className="h-screen flex flex-col">
-      <div className="flex grow">
-        <div className="px-3 py-1 border-r bg-gray-100 min-w-[240px] flex flex-col items-center">
-          <Link to="/">
-            <img src={LogoPng} width={120} height={120} />
-          </Link>
-          <ul className="m-0 p-1">
-            <li className="text-center"><Link to={`/`}>Home</Link></li>
-            <li className="text-center"><Link to={`/urlencoder`}>URL Encoder/Decoder</Link></li>
-            {/* <li><Link to={`/xmlparser`}>xml parser</Link></li> */}
-          </ul>
-          <div className="border my-3 ">
-            <DisplayAds.FixedVertical1 />
-          </div>
-        </div>
-        <div className="grow py-1 px-3">
-          <Outlet />
+
+  const menu = [
+    {
+      path: '/',
+      name: 'Home'
+    },
+    {
+      path: '/urlencoder',
+      name: 'URL Encoder/Decoder'
+    },
+    {
+      path: '/xmlparser',
+      name: 'XML Parser'
+    }
+  ];
+
+return (
+  <div className="h-screen flex flex-col">
+    <div className="flex grow">
+      <div className="px-3 py-1 border-r bg-gray-100 min-w-[240px] flex flex-col items-center">
+        <Link to="/">
+          <img src={LogoPng} width={120} height={120} />
+        </Link>
+        <ul className="m-0 p-1">
+          {
+            menu.map((m, i) => (
+              <li key={`menu-${i}`} className="text-center"><Link to={m.path}>{m.name}</Link></li>
+            ))
+          }
+        </ul>
+        <div className="border my-3 ">
+          <DisplayAds.FixedVertical1 />
         </div>
       </div>
-      <p className="p-1.5 m-0 border-t">
-        Go to <a href="https://github.com/bjtj" target="_blank" rel="noreferrer">Github</a>
-      </p>
+      <div className="grow py-1 px-3">
+        <Outlet />
+      </div>
     </div>
-  )
+    <p className="p-1.5 m-0 border-t">
+      Go to <a href="https://github.com/bjtj" target="_blank" rel="noreferrer">Github</a>
+    </p>
+  </div>
+            )
 }
