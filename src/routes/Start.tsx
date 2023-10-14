c4920433c086351f686e20122a3ab4f45f21e9fe
bjtj <bjtj10@gmail.com>
2023-10-14 16:54:00 +0900

    후원 링크

--- src/routes/Start.tsx
+++ src/routes/Start.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import TossLogoPng from '../assets/logo-toss-symbol-alpha.png';
+import PayPalLogoPng from '../assets/paypal_logo.png';
 
 export default function Start() {
 
@@ -20,7 +22,16 @@ export default function Start() {
     <div className="w-full h-full flex flex-col justify-center items-center">
       <h1 className="pointer-events-none select-none">Tools</h1>
       {myip && (<p><strong>Your IP: </strong><code className="inline text-sm">{myip}</code> (by
-      <a className="px-1 py-0.5 rounded bg-blue-200/50 border border-blue-500 mx-1" href="https://www.ipify.org/">ipfy</a>)</p>)}
+        <a className="px-1 py-0.5 rounded bg-blue-200/50 border border-blue-500 mx-1" href="https://www.ipify.org/">ipfy</a>)</p>)}
+      <div className="flex gap-3 my-1">
+        <a href="https://toss.me/uridongsu">
+          <img className="inline" src={TossLogoPng} width={20} height={20} />토스
+        </a>
+        <a href="https://paypal.me/bjtj10?country.x=KR&locale.x=ko_KR">
+          <img className="inline" src={PayPalLogoPng} width={20} height={20} />
+          PayPal
+        </a>
+      </div>
     </div>
   );
 }
